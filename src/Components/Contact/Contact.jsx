9a24c9f98c5da1c68eb0c127fb0f7c707c6dff57
Ehab-Tarek <ehabtarek166@gmail.com>
2023-12-12 19:45:42 +0200

    Finish Contact Section.

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
-
+import styles from './Contact.module.css'
 export default function Contact() {
   return (
-    <div>Contact</div>
+    <>
+      <section className='contact py-5 '>
+        <div className="container">
+          <div className="row ">
+            <h2 className='fw-bolder text-center text-uppercase'>contact section</h2>
+            <div className={`mb-5 text-center ${styles.star}`}>
+              <i className="fa-solid fa-star fa-2x"></i>
+            </div>
+
+            <div className="w-75 m-auto ">
+              <form action="">
+                <input type="text" className='w-100 form-control form-control-lg border-0 border-bottom rounded-0 mb-5' placeholder='UserName ' />
+                <input type="number" className='w-100 form-control form-control-lg border-0 border-bottom rounded-0 mb-5' placeholder='UserAge' />
+                <input type="text" className='w-100 form-control form-control-lg border-0 border-bottom rounded-0 mb-5' placeholder='UserEmail' />
+                <input type="password" className='w-100 form-control form-control-lg border-0 border-bottom rounded-0 mb-5' placeholder='UserPassword' />
+                <button className={`btn text-white ${styles.mainColor}`}>Send Message</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import styles from './NavBar.module.css'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 export default function NavBar() {
     return (
         <>
             <nav className={`navbar navbar-expand-lg py-4 ${styles.bgColor}`}>
                 <div className="container">
-                    <a className="navbar-brand fd-bold text-white" href="#">START FRAMEWORK</a>
+                    <Link className="navbar-brand fd-bold text-white" to={''}>START FRAMEWORK</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
@@ -18,7 +18,7 @@ export default function NavBar() {
                             <li className="nav-item">
                                 <Link className="nav-link fd-bold text-white me-3" to={'about'}>ABOUT</Link>
                             </li>
-                           
+
                             <li className="nav-item">
                                 <Link className="nav-link fd-bold text-white me-3" to={'portfolio'}>PORTFOLIO</Link>
                             </li>
