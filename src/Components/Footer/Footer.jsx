f1603e5a9ecda130a7d15d9ecb51643b5c47ed37
Ehab-Tarek <ehabtarek166@gmail.com>
2023-12-12 18:55:21 +0200

    Finish Home Section.

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,58 +3,59 @@ import styles from './Footer.module.css'
 export default function Footer() {
     return (
         <>
-            <footer className={`fixed-bottom pt-3 ${styles.bgColor} text-white fw-bold text-center`}>
+
+            <footer className={`text-center  text-white py-5  text-uppercase ${styles.bgColor}`}>
+
                 <div className="container">
-                    <div className="row">
+                    <div className="row align-items-center">
                         <div className="col-md-4">
-                            <div>
-                                <h2 className='mb-3'>LOCATION</h2>
-                                <p>2215 John Danial Drive</p>
-                                <p>Clark, MO 65243</p>
-                            </div>
+                            <h4 className='fw-bold mb-2'>Location</h4>
+                            <p>
+                                2215 John Daniel Drive
+                                <br />
+                                Clark, MO 65243
+                            </p>
                         </div>
 
-                        <div className="col-md-4">
-                            <div>
-                                <h2 className='mb-3'>
-                                    AROUND THE WORLD
-                                </h2>
-                                <div className='d-flex justify-content-center align-items-center'>
-                                    <div className={`${styles.icon} me-3`}>
-                                        <i className="fa-brands fa-facebook fa-1xl "></i>
-                                    </div>
-                                    <div className={`${styles.icon} me-3`}>
-                                        <i className="fa-brands fa-x-twitter fa-1xl "></i>
-                                    </div>
-                                    <div className={`${styles.icon} me-3`}>
-                                        <i className="fa-brands fa-linkedin fa-1xl "></i>
-                                    </div>
-                                    <div className={`${styles.icon} me-3`}>
-                                        <i className="fa-solid fa-earth-americas fa-1xl "></i>
-                                    </div>
+                        <div className="col-md-4 text-center d-flex flex-column justify-content-center align-items-center">
+                            <h4 className='fw-bold mb-4'>Around the Web</h4>
+                            <div className="icons d-flex text-center mb-3">
+                                <div className={`${styles.icon} me-2`}>
+                                    <i className="fa-brands fa-facebook-f fa-1x"></i>
+
+                                </div>
+
+                                <div className={`${styles.icon} me-2`}>
+                                    <i className="fa-brands fa-twitter fa-1x"></i>
+
+                                </div>
+
+                                <div className={`${styles.icon} me-2`}>
+                                    <i className="fa-brands fa-instagram fa-1x"></i>
+
+                                </div>
+
+                                <div className={`${styles.icon} me-2`}>
+                                    <i className="fa-brands fa-dribbble fa-1x"></i>
+
                                 </div>
                             </div>
                         </div>
 
                         <div className="col-md-4">
+                            <h4 className='fw-bold  mb-4'>About Freelancer</h4>
+                            <p>Freelance is a free to use, MIT
 
-                            <div>
-                                <h2 className='mb-3'>ABOUT FREELANCER</h2>
-                                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia temporibus libero tempore a cupiditate minus amet vero, reprehenderit maiores quasi ex dolor modi quis voluptas deleniti officiis iste soluta. Eveniet.</p>
-                            </div>
+                                <br />
+                                licensed Bootstrap theme created by Route </p>
                         </div>
-
-
-
                     </div>
-
-                </div>
-
-                <div className={`${styles.footerDown}  py-3  `}>
-                    <p>Copyright @ Your Website 2021</p>
                 </div>
             </footer>
 
+            <div className={`${styles.copyright} text-center text-white py-4`}>
+                Copyright © Your Website 2021
+            </div>
         </>
     )
 }
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
-
+import styles from './Home.module.css'
 export default function Home() {
   return (
-    <div>Home</div>
+    <>
+      <header className={`vh-100 d-flex justify-content-center align-items-center text-center  text-white`} style={{ backgroundColor: '#1ABC9C' }}>
+        <div>
+          <img src="./img/avataaars.svg" alt="" className='w-100 mb-2' />
+          <div className={styles.star}>
+            <i className="fa-solid fa-star fa-2xl"></i>
+          </div>
+          <h1>START PORTFOLIO</h1>
+          <p>Graphic Artist - Web Designer - Illustrator</p>
+        </div>
+      </header>
+    </>
   )
 }
